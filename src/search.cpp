afc5ea736d81325df6be8244625bf62d8f62b255
dsekercioglu <dsekercioglu2003@gmail.com>
2022-12-30 03:25:56 +0100

    0 reductions if move is killer

--- src/search.cpp
+++ src/search.cpp
@@ -152,7 +152,9 @@ struct Searcher {
                 }
                 reduction += legals > 3;
                 reduction -= history[board.stm == BLACK][piece][moves[i].to-A1] / 200;
-                reduction -= (killers[ply][0] == moves[i]) || (killers[ply][1] == moves[i]);
+                if ((killers[ply][0] == moves[i]) || (killers[ply][1] == moves[i])) {
+                    reduction = 0;
+                }
                 if (reduction < 0 || victim || in_check) {
                     reduction = 0;
                 }
